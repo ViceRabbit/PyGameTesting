768fdb050772c24657e019f43837ba5999d7e661
ViceRabbit <vicerabbit16789@gmail.com>
2022-09-04 22:53:31 -0400

    1

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+
+# VVVVV THIS right here initalizes pygame, this is importanto everytime
+pygame.init()
+
+# VVVVV this right here is the screen -- REMEMBER you gotta set like a tuple inside parameters: set_mode((like, this))
+gameScreen = pygame.display.set_mode((500, 500))
+
+# VVVVV so ik replit doesnt close window but its suggested so python doesnt automatically end the code/window
+# aka * Game Loop *
+active = True
+while active:
+  for event in pygame.event.get(): # we need a function to close the window!!!!
+    if event.type == pygame.QUIT: # so basically pygame.QUIT is the closing window event thing and we chec
+      print('Active went false! Code should be terminated')
+      active = False # window should be closed as well as the code
+
+# another git update from pycharm uwu
